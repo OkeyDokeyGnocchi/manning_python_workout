49f62c31eb510e0c3cb3b56054746a5e3b810db5
OkeyDokeyGnocchi <okeydokeygnocchi@gmail.com>
2024-02-24 14:04:45 -0800

    Added dict->csv function from extras & output file

--- Chapter5/22-rwcsv.py
+++ Chapter5/22-rwcsv.py
@@ -9,15 +9,28 @@ import os
 
 def passwd_to_csv(passwd_file, output_csv):
     with open(passwd_file, "r") as pf:
-        with open(output_csv, "w") as of:
-            writer = csv.writer(of, delimiter="\t")
+        with open(output_csv, "w") as out_file:
+            writer = csv.writer(out_file, delimiter="\t")
             for line in pf:
                 if not line.startswith(("#", "\n")):
                     writer.writerow([line.split(":")[0], line.split(":")[2]])
 
+def dict_to_csv(dictionary, output_csv):
+    with open(output_csv, "w") as out_file:
+        writer = csv.writer(out_file)
+        for k,v in dictionary.items():
+            writer.writerow([k, v, type(v).__name__])
 
 if __name__ == '__main__':
     passwd_file = os.path.dirname(__file__) + "/etc-passwd"
     csv_file = os.path.dirname(__file__) + "/22-ouput.csv"
 
     passwd_to_csv(passwd_file, csv_file)
+
+    val_dict = {
+        "root": 0,
+        "nobody": -2,
+        "Stringman": "StringmanUuid"
+    }
+    dict_csv = os.path.dirname(__file__) + "/22-dict.csv"
+    dict_to_csv(val_dict, dict_csv)
