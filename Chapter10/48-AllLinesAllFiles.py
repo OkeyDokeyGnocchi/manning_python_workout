2046bcdec8f9d1beb96795d19799e90761d7b90e
OkeyDokeyGnocchi <okeydokeygnocchi@gmail.com>
2024-03-17 14:44:48 -0700

    Added 48 modded generator function with string filtering option

--- Chapter10/48-AllLinesAllFiles.py
+++ Chapter10/48-AllLinesAllFiles.py
@@ -14,12 +14,32 @@ def all_lines(walk_dir):
                         for line in f:
                             # Added .strip() to stop all the newlines
                             yield line.strip()
-            except OsError:
+            except OSError:
                 pass
 
+def all_lines_modded(walk_dir, find_string=''):
+    # Trying a different method to get files
+    for dir, subdir, files in os.walk(walk_dir):
+        for file_idx, file in enumerate(files):
+            try:
+                with open(os.path.join(walk_dir, file)) as f:
+                    for line_idx, line in enumerate(f):
+                        if str(find_string) in line:
+                            yield (file, file_idx, line_idx, line)
+                        line_idx += 1
+            except OSError:
+                pass
 
 if __name__ == '__main__':
     file_dir = os.path.dirname(__file__)
     walk_dir = file_dir + "/files"
     for line in all_lines(walk_dir):
-         print(line)
+        print(line)
+    
+    # Optionals
+    print()
+    for line in all_lines_modded(walk_dir):
+        print(line)
+    print("## Modded with find_string ##")
+    for line in all_lines_modded(walk_dir, 1):
+        print(line)
