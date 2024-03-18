1158c5c33f4bc5cbbd75082df286abec7fbd1a66
OkeyDokeyGnocchi <okeydokeygnocchi@gmail.com>
2024-03-17 17:49:36 -0700

    Added 49 optional gen function file_usage_timing for getting file access/mod/create times. Modified files/01.txt for changed timestamp test

--- Chapter10/49-ElapsedSince.py
+++ Chapter10/49-ElapsedSince.py
@@ -5,6 +5,7 @@
 #    - Should yield tuple: (int(seconds_since_last_iter), iterable_element)
 #    - Recommended to use time.perf_counter()
 
+import os
 import random
 import time
 
@@ -17,6 +18,9 @@ def elapsed_since_gen(iterable):
         yield (time_change, item)
 
 # Optionals
+# Minimum sleep timer
+#  - Add a minimum sleep timer to elapsed_since
+#    - Should sleep if time elapsed < min_sleep
 def elapsed_since_gen_mk2(iterable, min_sleep):
     last_time = 0
     for item in iterable:
@@ -28,7 +32,20 @@ def elapsed_since_gen_mk2(iterable, min_sleep):
         time_change = item_time - last_time
         last_time = time.perf_counter()
         yield (time_change, item)
-        
+
+# File usage timing function
+#  - For file in directory:
+#    - Should give access time, mod time, creation time
+def file_usage_timing(directory):
+    for file in os.listdir(directory):
+        filename = os.path.join(directory, file)
+        try:
+            stats = os.stat(filename)
+            atime, mtime, ctime = stats.st_atime, stats.st_mtime, stats.st_ctime
+            yield f'{filename}:\nAccess Time: {atime}, Mod Time: {mtime}, Creation Time: {ctime}\n'
+        except OSError:
+            pass
+
 
 if __name__ == '__main__':
     """
@@ -39,7 +56,15 @@ if __name__ == '__main__':
     """
     
     # Optionals
+    # Minimum sleep elapsed_since_gen
+    """
     for char in elapsed_since_gen_mk2('123456', 3):
         print(char)
         sleep_time = random.randint(2, 6)
         time.sleep(sleep_time)
+    """
+
+    # file_usage_timing
+    directory = os.path.dirname(__file__) + "/files"
+    for file in file_usage_timing(directory):
+        print(file)
