067084bc3f5193a104cd1ddc6557ece7139e2b90
OkeyDokeyGnocchi <okeydokeygnocchi@gmail.com>
2024-03-18 21:28:53 -0700

    Added 50 first beyond exercise: myzip generator function

--- Chapter10/50-MyChain.py
+++ Chapter10/50-MyChain.py
@@ -9,7 +9,26 @@ def mychain(*args):
         for item in iterable:
             yield item
 
+# Beyond the exercise
+# Recreate zip()
+def myzip(first_iterable, second_iterable):
+    # Should only go for length of the shorter of the two inputs
+    if len(first_iterable) > len(second_iterable):
+        iter_size = len(second_iterable)
+    else:
+        iter_size = len(first_iterable)
+    for i in range(iter_size):
+        yield (first_iterable[i], second_iterable[i])
+
 
 if __name__ == '__main__':
     for item in mychain('abc', [1,2,3], {'a':1, 'b':2}):
         print(item)
+    
+    # myzip
+    for pair in myzip('abc', [1,2,3]):
+        print(pair)
+    for pair in myzip('cba', [1,2]):
+        print(pair)
+    for pair in myzip('def', [5,6,7,8]):
+        print(pair)
